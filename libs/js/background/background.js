437598e6e89670cc90ca273f800946c4566d75b6
yubol <yubol@splunk.com>
2018-07-10 10:07:08 +0800

    Ui refine and bug fix

--- libs/js/background/background.js
+++ libs/js/background/background.js
@@ -1,6 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({convert_color: false}, function() {
-    });
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
@@ -13,18 +11,5 @@ chrome.runtime.onInstalled.addListener(function() {
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
-    });
-
-    chrome.identity.getProfileUserInfo(function(userInfo) {
-        let user_info_url = "http://picasaweb.google.com/data/entry/api/user/" + "117726327892945278137" + "?alt=json";
-        let xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", user_info_url, false);
-        xmlHttp.send(null);
-        let up = JSON.parse(xmlHttp.responseText).entry["gphoto$thumbnail"]["$t"];
-        chrome.storage.sync.set({user_profile_photo_url: up }, function() {
-        });
-        let un = JSON.parse(xmlHttp.responseText).entry["gphoto$nickname"]["$t"];
-        chrome.storage.sync.set({user_profile_name: un }, function() {
-        });
     });
 });
---
+++ libs/js/popup/load_settings.js
@@ -0,0 +1,25 @@
+chrome.storage.sync.set({convert_color: false}, function() {
+});
+
+chrome.identity.getProfileUserInfo(function(userInfo) {
+    let user_info_url = userInfo.id;
+    if (user_info_url){
+        let xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", user_info_url, false);
+        xmlHttp.send(null);
+        let up = JSON.parse(xmlHttp.responseText).entry["gphoto$thumbnail"]["$t"];
+        chrome.storage.sync.set({user_profile_photo_url: up}, function () {
+        });
+        let un = JSON.parse(xmlHttp.responseText).entry["gphoto$nickname"]["$t"];
+        chrome.storage.sync.set({user_profile_name: un}, function () {
+        });
+    }
+    else
+    {
+        // set default if not login
+        chrome.storage.sync.set({user_profile_photo_url: '../images/splunk-ninja.png'}, function () {
+        });
+        chrome.storage.sync.set({user_profile_name: 'splunkie'}, function () {
+        });
+    }
+});

1ca1440f488715b600302607b5c212a1eaa851a8
lwei <lwei@lweis-mbp.sv.splunk.com>
2018-07-12 18:06:42 +0800

    merge roman pr

--- libs/js/popup/load_settings.js
+++ libs/js/popup/load_settings.js
@@ -5,11 +5,22 @@ chrome.identity.getProfileUserInfo(function(userInfo) {
         xmlHttp.open("GET", user_info_url, false);
         xmlHttp.send(null);
         let up = JSON.parse(xmlHttp.responseText).entry["gphoto$thumbnail"]["$t"];
-        chrome.storage.sync.set({user_profile_photo_url: up}, function () {
-        });
         let un = JSON.parse(xmlHttp.responseText).entry["gphoto$nickname"]["$t"];
-        chrome.storage.sync.set({user_profile_name: un}, function () {
-        });
+
+        if(up && un){
+            chrome.storage.sync.set({user_profile_photo_url: up}, function () {
+            });
+
+            chrome.storage.sync.set({user_profile_name: un}, function () {
+            });
+        }
+        else{
+            // set default if not login
+            chrome.storage.sync.set({user_profile_photo_url: '../images/splunk-ninja.png'}, function () {
+            });
+            chrome.storage.sync.set({user_profile_name: 'splunkie'}, function () {
+            });
+        }
     }
     else
     {
